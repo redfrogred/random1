f146ab02c015e13793181d4f211e74bc1c6bcfb2
Matt <matt.garvin.416@gmail.com>
2022-06-11 12:00:56 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.blue,
+            child: const Center(
+              child: 
+              Text('hello squirrel'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
